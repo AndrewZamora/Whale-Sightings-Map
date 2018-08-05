f68cc345967d2920c805781df56199b3b609cd0d
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-08-05 19:15:29 +0900

    Added target blank for footer link to open up in seperate tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Footer.css';
-   
+
 const footerStyle = {
-    "display":"flex",
+    "display": "flex",
     "height": "100%"
 };
-const pStyle = {"margin":"auto 0 0.3em 0.3em"}
-const aStyle = {"textDecoration":"none","fontWeight":"bold","color":"#50C878"};
+const pStyle = { "margin": "auto 0 0.3em 0.3em" }
+const aStyle = { "textDecoration": "none", "fontWeight": "bold", "color": "#50C878" };
 
 
-const Footer =()=>(
-            <footer style={footerStyle}>
-                <p style={pStyle}>Data provided by  <a 
-                style={aStyle}
-                href="https://whalemuseum.org/">The Whale Museum</a> and code by Andrew Zamora</p>
-            </footer>
-        );
+const Footer = () => (
+    <footer style={footerStyle}>
+        <p style={pStyle}>Data provided by  <a
+            style={aStyle}
+            href="https://whalemuseum.org/" target="_blank"
+            rel="noopener noreferrer">The Whale Museum</a> and code by Andrew Zamora</p>
+    </footer>
+);
 
 export default Footer;
