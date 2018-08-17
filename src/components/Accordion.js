d9ee86ff02793563141c04bd8ad8df0783194015
AndrewZamora <andrewmichaelzamora@gmail.com>
2018-08-17 21:52:03 +0900

    Added styling to ul elements in Accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,11 +5,17 @@ import React, { Component } from 'react';
 
 class Accordion extends Component {
     render() {
+        const ulStyle = {
+            "listStyle": "none",
+            "margin": "0.2em 0 0.2em 0.3em",
+            "padding": "0",
+            "text-transform":"capitalize"
+        };
         const allLinks = this.props.links[this.props.num].map(link => {
             return <li key={link}><a>{link}</a></li>
         })
         return (
-         <ul>{allLinks}</ul>
+         <ul style={ulStyle}>{allLinks}</ul>
         );
     }
 
