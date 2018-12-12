edec5b268269f84892e23533879e2bfc933e4095
Andrew Zamora <andrewmichaelzamora@gmail.com>
2018-12-12 14:02:48 +0900

    Used a template literal in fetch request

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   }
 
   getSpeciesData = animal => {
-    fetch('https://hotline.whalemuseum.org/api.json?species=' + animal, {mode: 'cors'})
+    fetch(`https://hotline.whalemuseum.org/api.json?species=${animal}`, {mode: 'cors'})
       .then(response => response.json())
       .then((data) => {
         this.setState({
